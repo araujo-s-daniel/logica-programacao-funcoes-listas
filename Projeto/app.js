09fdd2ad42197256a02faacdf6aff38df28d213c
Daniel Sena <daniels_araujo@outlook.com.br>
2025-06-09 13:59:00 -0300

    03.03 - Contando tentativas

--- Projeto/app.js
+++ Projeto/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -12,13 +13,16 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('p', mensagemTentativas);
     } else {
         if (numeroSecreto < chute) {
             exibirTextoNaTela('p', 'O número secreto é menor do que o chute');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior do que o chute');
         }
+        tentativas++;
     }
 }
 
