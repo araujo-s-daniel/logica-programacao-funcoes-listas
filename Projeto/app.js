c00e01373819438b65e0acc60b493770853d635a
Daniel Sena <daniels_araujo@outlook.com.br>
2025-06-09 13:42:37 -0300

    03.02 - Verificando o chute

--- Projeto/app.js
+++ Projeto/app.js
@@ -10,7 +10,16 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto');
+    } else {
+        if (numeroSecreto < chute) {
+            exibirTextoNaTela('p', 'O número secreto é menor do que o chute');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior do que o chute');
+        }
+    }
 }
 
 function gerarNumeroAleatorio() {
